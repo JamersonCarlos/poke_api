8cd12cb833ef2d6cf51c814fdc765d865f68b085
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-05-08 18:54:37 -0300

    custom hook for api

---
+++ src/hooks/apiServer.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+export const useFetch = (url) => { 
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState();
+    const [images, setImages] = useState([]);
+
+    useEffect(() => { 
+        async function getAllPokemons() { 
+            setImages([]);
+            setLoading(true);
+            const response = await fetch(url);
+            const responseJson = await response.json();
+            const resultJson = await responseJson.results;
+            resultJson.map( async (item, index) => {
+                const res = await fetch(item.url);
+                const json = await res.json();
+                setImages((prevValue) => [...prevValue, json.sprites.front_default]);
+            });
+            setData(resultJson);
+            setLoading(false);            
+        }  
+
+        getAllPokemons();
+
+    }, [url]);
+
+    console.log(images)
+
+    return {data, loading, images}
+}
