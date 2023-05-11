a53b4e58ff43a9acf5e306ca23640c18a5a0650b
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-05-11 10:16:54 -0300

    implementation search through pokemon names

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useFetch } from '../hooks/apiServer'
 import './PokeList.css';
 
 function PokeList() {
     const {data: items, loading, images} = useFetch("https://pokeapi.co/api/v2/pokemon/?limit=42");
+    const [searchInput, setSearchInput] = useState();
     function randomColor(opacidade = 1) { 
         let r = Math.random() * 255; 
         let g = Math.random() * 255; 
@@ -14,14 +15,26 @@ function PokeList() {
   return (
     <div>
         <h1>PokeDex</h1>
+        <input type="text" name='search' onChange={(e) => setSearchInput(e.target.value)} placeholder='Search Pokemon'/>
         <div className='grid_pokemons'>
             {items != null && 
-                items.map((pokemon, index) => (
-                    <div key={index} className='card_pokemon' style={{backgroundColor: randomColor()}}>
-                        <img style={{width: 120}} src={images[index]} alt="" />
-                        <p>{pokemon.name}</p>
-                    </div>
-                ))
+                items.map((pokemon, index) => {
+                    if (!searchInput) { 
+                        return (
+                        <div key={index} className='card_pokemon' style={{backgroundColor: 'rgb(79, 118, 3)'}}>
+                            <img style={{width: 120}} src={images[index]} alt=""/>
+                            <p>{pokemon.name}</p>
+                        </div>)
+                    } else { 
+                        if (Object.values(pokemon)[0].split('').join('').includes(searchInput.toLowerCase())) { 
+                            return (
+                            <div key={index} className='card_pokemon' style={{backgroundColor: 'rgb(79, 118, 3)'}}>
+                                <img style={{width: 120}} src={images[index]} alt=""/>
+                                <p>{pokemon.name}</p>
+                            </div>)
+                        } 
+                    }
+                })
             }
         </div>
     </div>
