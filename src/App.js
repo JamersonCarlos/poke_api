0d04b530e2eba54b4e04cafd0b7f2361c1daf93b
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-05-12 01:09:18 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import PokeList from './components/PokeList';
 
+//Initialize first configuration
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import PokeInfo from './pages/PokeInfo';
+
 function App() {
   return (
     <div className="App">
-      <PokeList/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<PokeList/>}></Route>
+          <Route path='/pokemons/:id' element={<PokeInfo/>}></Route>
+        </Routes>      
+      </BrowserRouter>
     </div>
   );
 }
