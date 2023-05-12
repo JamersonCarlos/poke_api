343f26ead53295372003d8807a167002d333353c
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-05-12 01:33:46 -0300

    details page

--- src/pages/PokeInfo.js
+++ src/pages/PokeInfo.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
+import PokeStats from '../components/PokeStats';
+import './PokeInfo.css';
 
 function PokeInfo() {
   const {id} = useParams();
   const [name, setName] = useState();
   const [urlImg, setImg] = useState();
+  const [statistics, setStatistics] = useState();
   
   useEffect(() => { 
     async function getInfoPokemon() { 
@@ -13,6 +15,7 @@ function PokeInfo() {
       const responseJson = await response.json();
       setName(responseJson.forms[0].name);
       setImg(responseJson.sprites.front_default);
+      setStatistics(responseJson.stats);
     }
 
     getInfoPokemon();
@@ -24,6 +27,7 @@ function PokeInfo() {
       <div className='infoPokemon'>
         <h3 className='namePoke'>{name}</h3>
         <img src={urlImg} alt="" />    
+        <PokeStats statistics={statistics} typeListStats={false}/>
       </div>
     </div>  
   )
